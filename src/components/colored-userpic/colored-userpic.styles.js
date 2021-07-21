42a9fcef052f9f61b4967a791bea499298911d51
Aleksey <alexey-step@mail.ru>
2021-07-22 03:52:39 +0500

    refactor (colored component styles)

--- src/components/colored-userpic/colored-userpic.styles.js
+++ src/components/colored-userpic/colored-userpic.styles.js
@@ -4,15 +4,15 @@ export const GradientCircle = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: ${(props) => props.size || 100}px;
-  height: ${(props) => props.size || 100}px;
+  width: ${({ size }) => size || 100}px;
+  height: ${({ size }) => size || 100}px;
   overflow: hidden;
-  background: ${(props) => `linear-gradient(to right, ${props.colors[0]}, ${props.colors[1]})`};
-  padding: ${(props) => props.colorWidth || 5}px;
+  background: ${({ colors }) => `linear-gradient(to right, ${colors[0]}, ${colors[1]})`};
+  padding: ${({ colorWidth }) => colorWidth || 2}px;
   border-radius: 50%;
 
   &:hover {
-    background: ${(props) => `linear-gradient(to right, ${props.hoverColors[0]}, ${props.hoverColors[1]})`};
+    background: ${({ hoverColors }) => `linear-gradient(to right, ${hoverColors[0]}, ${hoverColors[1]})`};
   }
 `
 
@@ -20,8 +20,8 @@ export const ImgWrapper = styled.div`
   width: 100%;
   height: 100%;
   border-radius: 50%;
-  padding: ${(props) => props.margin || 4}px;
-  background-color: ${(props) => props.backgroundColor || 4};
+  padding: ${({ margin }) => margin || 2}px;
+  background-color: ${({ backgroundColor }) => backgroundColor || "white"};
   overflow: hidden;
 `
 
